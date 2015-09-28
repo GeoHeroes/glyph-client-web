143a61cd3ee49fa1ba6154a3ed833506bc8db721
Richard Artoul <richardartoul@gmail.com>
2015-09-27 21:36:23 -0700

    [Feature] Added a modal to the application

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
 var React = require('react');
 var Map = require('./map');
+var Modal = require('boron/DropModal');
 
 var App = React.createClass({
+  showModal: function() {
+    this.refs.modal.show();
+  },
+
+  componentDidMount: function() {
+    this.showModal();
+  },
+
   render: function() {
     return <div>
       <h1 className="red">
         GlyphBase!
       </h1>
       <Map latitude={25} longitude={25}></Map>
+      <Modal ref="modal" id="modal">
+        <h1> Yolo Motherfucker! </h1>
+      </Modal>
     </div>
   }
 });
